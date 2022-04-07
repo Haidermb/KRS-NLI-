7bc408846e628b55ab0a10f462b0771f933d47c5
Haider.b <93571887+Haidermb@users.noreply.github.com>
2022-04-07 21:56:54 +0530

    Update and rename (sqlalchemy)app.py to (sqlalchemy working2)app3.py

--- (sqlalchemy working2)app3.py
+++ (sqlalchemy working2)app3.py
@@ -1,6 +1,7 @@
 from flask import Flask , render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import func
+
 app= Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///krtest2.sqlite'
  
@@ -14,16 +15,15 @@ class students(db.Model):
    btime = db.Column(db.DateTime(timezone=True), server_default=func.now())
    rname = db.Column('Returner_Name',db.String(200))
    rtime = db.Column(db.DateTime(timezone=True), server_default=func.now())
-def __init__(self, roomno, bname, rname):
-   self.room = roomno
+def __init__(self, room, bname, rname):
+   self.room = room
    self.bname = bname
    self.rname = rname
 
 @app.route("/h" , methods = ['POST', 'GET'])
 def add():
    if request.method == "POST":
-    
-        b=students(request.form['roomno'],request.form['bname'],request.form['rname'] ) # not working 
+        b=students(room=request.form['roomno'],bname=request.form['bname'],rname=request.form['rname'])    
         db.session.add(b)
         db.session.commit()
         return render_template('home.html')
@@ -31,7 +31,7 @@ def add():
 @app.route('/')
 def form():
     return render_template('home.html')
-
+ 
 if __name__=="__main__":
     app.run(debug=True)
 
