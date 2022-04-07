33e7350905d1fca795f4b3b241e715823459cdf8
Haider.b <93571887+Haidermb@users.noreply.github.com>
2022-04-07 21:42:14 +0530

    Create (sqlalchemy)app.py

---
+++ (sqlalchemy)app.py
@@ -0,0 +1,37 @@
+from flask import Flask , render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import func
+app= Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///krtest2.sqlite'
+ 
+# creating object of class SQLAlchemy with app as parameter 
+
+db = SQLAlchemy(app)
+class students(db.Model):
+   room= db.Column('Room_No',db.Integer, primary_key = True )
+   date = db.Column('Date',db.Date, default=db.func.now())
+   bname = db.Column('Borrower_Name',db.String(200))
+   btime = db.Column(db.DateTime(timezone=True), server_default=func.now())
+   rname = db.Column('Returner_Name',db.String(200))
+   rtime = db.Column(db.DateTime(timezone=True), server_default=func.now())
+def __init__(self, roomno, bname, rname):
+   self.room = roomno
+   self.bname = bname
+   self.rname = rname
+
+@app.route("/h" , methods = ['POST', 'GET'])
+def add():
+   if request.method == "POST":
+    
+        b=students(request.form['roomno'],request.form['bname'],request.form['rname'] ) # not working 
+        db.session.add(b)
+        db.session.commit()
+        return render_template('home.html')
+
+@app.route('/')
+def form():
+    return render_template('home.html')
+
+if __name__=="__main__":
+    app.run(debug=True)
+
